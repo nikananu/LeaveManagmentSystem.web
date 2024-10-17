47bddf22537b0a8004a83efea2d4d0c375278f72
nikananu <n_nanuashvili@cu.edu.ge>
2024-10-17 13:00:13 +0400

    Add project files.

---
+++ LeaveManagmentSystem.web/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeaveManagmentSystem.web.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet <LeaveType> LeaveTypes { get; set; }
+    }  
+
+
+}
---
+++ LeaveManagmentSystem.web/Data/LeaveType.cs
@@ -0,0 +1,9 @@
+﻿namespace LeaveManagmentSystem.web.Data
+{
+    public class LeaveType
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public int NumberOfDays { get; set; }
+    }
+}
